6e8452d8125030fff691c69b22e1e6d85e258bff
NamHuyNguyenKhac <21020542@vnu.edu.vn>
2023-07-26 09:56:07 +0700

    B1 B2 LTNC-06

---
+++ Hackerrank/LTNC-06/B2.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+/*
+Enter code for class Student here.
+Read statement for specification.
+*/
+class Student {
+    private:
+        int age;
+        string first_name;
+        string last_name;
+        int standard;
+    public:
+        void set_age(int a) {
+            age = a;
+        }
+        int get_age() {
+            return age;
+        }
+        void set_standard(int a) {
+            standard = a;
+        }
+        int get_standard() {
+            return standard;
+        }
+        void set_first_name(string a) {
+            first_name = a;
+        }
+        string get_first_name() {
+            return first_name;
+        }
+        void set_last_name(string a) {
+            last_name = a;
+        }
+        string get_last_name() {
+            return last_name;
+        }
+        string to_string(){
+            ostringstream str1;
+            ostringstream str2;
+            str1 << age;
+            str2 << standard;
+            string age_str, standard_str;
+            age_str = str1.str();
+            standard_str = str2.str();
+            return age_str + "," + first_name + "," + last_name + "," + standard_str;
+        }
+};
+
+int main() {
+    int age, standard;
+    string first_name, last_name;
+
+    cin >> age >> first_name >> last_name >> standard;
+
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+
+    cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+
+    return 0;
+}
