8be16f02434850cd83296a03676af4672bf91a28
NamHuyNguyenKhac <21020542@vnu.edu.vn>
2023-10-03 18:16:07 +0700

    Update B4.cpp

--- Hackerrank/LTNC-07/B4.cpp
+++ Hackerrank/LTNC-07/B4.cpp
@@ -3,9 +3,19 @@
 using namespace std;
 
 int main() {
-    string s;
-    cin >> s;
-    s.clear();
-    cin >> s;
-    cout << s;
+    string k, words = "ICELAND;MEXICO;PANAMA;ALMATY";
+    vector<string> wordList;
+    k.clear();
+    for(int i = 0; i < (int)words.length(); i++) {
+        if(words[i] == ';' || i == words.length()-1) {
+            wordList.push_back(k);
+            k.clear();
+        }
+        else {
+            k += words[i];
+        }
+    }
+    for(int i = 0; i < wordList.size(); i++) {
+        cout << wordList.at(i) << "\n";
+    }
 }
